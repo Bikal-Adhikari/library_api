3729a29f1c1ddd1940564a65621dfaaa658e1e32
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-06-25 18:10:29 +1000

    Cors option added to allow frontend server

--- server.js
+++ server.js
@@ -8,26 +8,14 @@ const PORT = process.env.PORT || 8000;
 import { connectMongoDB } from "./src/config/monogoConfig.js";
 connectMongoDB();
 
-// middlewares
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://library-client-xi.vercel.app/",
-// ];
+const corsOptions = {
+  origin: ["https://library-client-xi.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 204,
+};
 
-// // Configure CORS
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (like mobile apps, curl requests)
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg =
-//         "The CORS policy for this site does not allow access from the specified Origin.";
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
+app.options("*", cors(corsOptions));
 
 app.use(cors()); // Apply CORS middleware globally
 app.use(express.json());
